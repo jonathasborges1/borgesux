e8622b4bcbcfe16e610e3556c36b8448f4d2187b
jonathasborges1 <jbc@icomp.ufam.edu.br>
2022-01-18 01:41:59 -0400

    v0.0.3

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,9 +1,10 @@
 
 import {Container, Row, Col} from 'react-bootstrap'
 
-import { Paper, Grid, styled} from '@mui/material'
+import { Paper, Grid, styled, Box, Icon} from '@mui/material'
 import { makeStyles } from '@material-ui/styles'
 //import { Logotipo } from '../assets/logobrand'
+import StarIcon from '@mui/icons-material/Star';
 
 const Logotipo = require('../assets/logobrand.png')
 
@@ -30,44 +31,51 @@ export default function Header(){
     const classes = useStyles();
     return(
         <>
-        <Grid>
-            <Grid  className={`${classes.root} ${'d-frex'}`}  alignItems='center'>
-                <Item >
-                    <img src={Logotipo} alt="Logotipo" />
-                </Item >
-                <Item >
-                    <h6>BorgesUX</h6>
-                </Item>
+        <Grid container  style={{height: '100px',}} >
+
+            <Grid item style={{ display: 'flex', alignItems: 'center',alignSelf:'center',height: '50px', }} >
+                <img src={Logotipo} alt="logotipo" style={{borderRadius:'50%',height: '72px',width:'auto'}} />
+                <h6 style={{marginLeft:'-10px'}} >Borges<strong style={{color: '#6AD8B1'}} >UX</strong> </h6>
+            </Grid>
+            <Grid item flexGrow='1'/>
+            <Grid item direction="column" spacing={2} style={{display:'flex',alignItems:'center'}} className='m-2'>
+                <div style={ {color:'#6AD8B1'} }>
+                    <StarIcon  /> <StarIcon/> <StarIcon/> <StarIcon/> <StarIcon/>
+                </div> 
+                <strong>5 Estrelas</strong>
             </Grid>
 
-            <Grid item xs="auto">
-                <Item>xs=6</Item>
-            </Grid>
         </Grid>
-
-
-        <Container> 
-                <Row className="px-1 mx-0" style={myContainerRow}>
-
-                    <Col className='py-4 px-1 mx-0' style={myContainerRowCol} xs={6} sm={4} md={4} lg={3}> 
-                        <span className='align-items-end' style={myElement}>bloco 2</span> 
-                    </Col>
-
-                    <Col className='py-4 px-1 mx-2 my-2' style={myContainerRowCol} xs={5} >
-                        <div  >
-                            <span style={landingtopv2header}>bloco 3</span>
-                        </div>
-                    </Col>
-                </Row>
-        </Container>
-
- 
         </>
     )
 }
 
 
 const useStyles = makeStyles((theme) => ({
+    grid1: {
+        border: '2px solid red',
+        
+    },
+    grid2: {
+        border: '2px solid yellow',
+        display: 'inline-flex',
+        alignItems: 'center',
+        alignSelf: "center" ,
+    },
+    item1: {
+        border: '2px solid blue',
+        //alignItems: 'flex-start',
+    },
+    img1: {
+        border: '2px solid purple',
+    },
+    imgLogotipo: {
+        border: '5px solid purple',
+        borderRadius: '50%',
+        background: 'black',
+        width: '52',
+        height: '52',
+    },
     root: {
         background: 'black',
         display: 'flex',
@@ -81,6 +89,7 @@ const useStyles = makeStyles((theme) => ({
         display: "flex",
         justifyContent: "space-between",
       },
+
 }));
 
 const Item = styled(Paper)(({ theme }) => ({
